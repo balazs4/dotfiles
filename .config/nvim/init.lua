f8b5fd907e56c8ead7f12e29b9d69186a8ed1c18
balazs4 <balazs4@users.noreply.github.com>
2022-07-31 15:48:30 +0200

    1659275310@carbon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,19 @@
 -- Linux Darwin
 
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- https://github.com/Shatur/neovim-ayu
+require('ayu').colorscheme()
+
 -- https://github.com/neovim/nvim-lspconfig
+require('lspconfig')['gopls'].setup({
+	on_attach = function(client, bufnr)
+    vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, {noremap=true, silent=true, buffer=bufnr})
+	end
+})
+
+
+-- https://github.com/ibhagwan/fzf-lua
