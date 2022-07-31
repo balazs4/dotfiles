c5c9140e17640a4384e54a5ab93caedf761a52f5
balazs4 <balazs4@users.noreply.github.com>
2022-07-31 22:14:43 +0200

    1659298483@carbon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,18 +6,30 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.guicursor = 'i:block'
 
--- https://github.com/Shatur/neovim-ayu
-require('ayu').colorscheme()
-
 -- https://github.com/ibhagwan/fzf-lua
 vim.keymap.set('n', '<c-P>', require('fzf-lua').files, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>[', require('fzf-lua').buffers, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader><leader>', require('fzf-lua').builtin, { noremap = true, silent = true })
 
 -- https://github.com/neovim/nvim-lspconfig
+vim.diagnostic.config({virtual_text = false, signs = false})
 require('lspconfig')['gopls'].setup({
 	on_attach = function(client, bufnr)
     vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', 'gR', vim.lsp.buf.rename, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', '<leader>p', vim.lsp.buf.formatting, {noremap=true, silent=true, buffer=bufnr})
     vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', 'ga', require('fzf-lua').lsp_code_actions, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', '<cr><cr>', require('fzf-lua').lsp_document_diagnostics, {noremap=true, silent=true, buffer=bufnr})
 	end
 })
 
+-- https://github.com/Shatur/neovim-ayu
+vim.cmd 'colorscheme ayu'
+
+-- https://github.com/Mofiqul/dracula.nvim
+vim.cmd 'colorscheme dracula'
+
+-- https://github.com/prettier/vim-prettier
+vim.keymap.set('n', '<leader>p', ':PrettierAsync<CR>', { noremap = true, silent = true })
