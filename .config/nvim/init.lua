dacc1870e744de804f37a38f7d0130a1b9ce2c89
balazs4 <balazs4@users.noreply.github.com>
2022-08-01 10:49:34 +0200

    1659343774@macbookpro

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,11 +25,23 @@ require('lspconfig')['gopls'].setup({
 	end
 })
 
+require('lspconfig')['tsserver'].setup({
+	on_attach = function(client, bufnr)
+    vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', 'gR', vim.lsp.buf.rename, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', 'ga', require('fzf-lua').lsp_code_actions, {noremap=true, silent=true, buffer=bufnr})
+    vim.keymap.set('n', 'gb', require('fzf-lua').lsp_document_diagnostics, {noremap=true, silent=true, buffer=bufnr})
+	end
+})
 -- https://github.com/Shatur/neovim-ayu
 vim.cmd 'colorscheme ayu'
 
--- https://github.com/Mofiqul/dracula.nvim
-vim.cmd 'colorscheme dracula'
-
 -- https://github.com/prettier/vim-prettier
 vim.keymap.set('n', '<leader>p', ':PrettierAsync<CR>', { noremap = true, silent = true })
+
+-- https://github.com/leafgarland/typescript-vim
+
+-- https://github.com/jelera/vim-javascript-syntax
+
