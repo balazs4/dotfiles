abb47fb6f1ad5dfb39b635c4c2d7b3a44e616acb
balazs4 <balazs4@users.noreply.github.com>
2023-02-10 16:06:08 +0100

    1676041568@macbookpro

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,11 +20,23 @@ vim.keymap.set('n', '<leader>[', require('fzf-lua').buffers, { noremap = true, s
 vim.keymap.set('n', '<leader><leader>', require('fzf-lua').builtin, { noremap = true, silent = true })
 vim.keymap.set('n', '``', require('fzf-lua').resume, { noremap = true, silent = true })
 
+-- https://github.com/hrsh7th/nvim-cmp/
+-- https://github.com/hrsh7th/cmp-nvim-lsp/
+-- https://github.com/hrsh7th/cmp-vsnip
+-- https://github.com/hrsh7th/vim-vsnip
+require'cmp_nvim_lsp'.setup {
+  snippet = { expand = function(args) vim.fn["vsnip#anonymous"](args.body) end },
+  sources = { { name = 'nvim_lsp' }, {name='vsnip'} },
+}
+
+
 -- https://github.com/neovim/nvim-lspconfig
 vim.diagnostic.config({virtual_text = true, signs = false, update_in_insert = false})
 
 require('lspconfig')['gopls'].setup({
 	on_attach = function(client, bufnr)
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
     vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, {noremap=true, silent=true, buffer=bufnr})
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap=true, silent=true, buffer=bufnr})
     vim.keymap.set('n', 'gR', vim.lsp.buf.rename, {noremap=true, silent=true, buffer=bufnr})
@@ -37,6 +49,7 @@ require('lspconfig')['gopls'].setup({
 
 require('lspconfig')['rust_analyzer'].setup({
 	on_attach = function(client, bufnr)
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
     vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, {noremap=true, silent=true, buffer=bufnr})
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap=true, silent=true, buffer=bufnr})
@@ -50,6 +63,7 @@ require('lspconfig')['rust_analyzer'].setup({
 
 require('lspconfig')['tsserver'].setup({
 	on_attach = function(client, bufnr)
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
     vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, {noremap=true, silent=true, buffer=bufnr})
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap=true, silent=true, buffer=bufnr})
@@ -59,37 +73,6 @@ require('lspconfig')['tsserver'].setup({
     vim.keymap.set('n', 'gb', require('fzf-lua').lsp_document_diagnostics, {noremap=true, silent=true, buffer=bufnr})
 	end
 })
-
--- require('lspconfig')['sumneko_lua'].setup({
--- capabilities = require('cmp_nvim_lsp').default_capabilities(),
--- 	on_attach = function(client, bufnr)
---     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---     vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, {noremap=true, silent=true, buffer=bufnr})
---     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap=true, silent=true, buffer=bufnr})
---     vim.keymap.set('n', 'gR', vim.lsp.buf.rename, {noremap=true, silent=true, buffer=bufnr})
---     vim.keymap.set('n', '<leader>p', vim.lsp.buf.formatting, {noremap=true, silent=true, buffer=bufnr})
---     vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references, {noremap=true, silent=true, buffer=bufnr})
---     vim.keymap.set('n', 'ga', require('fzf-lua').lsp_code_actions, {noremap=true, silent=true, buffer=bufnr})
---     vim.keymap.set('n', 'gb', require('fzf-lua').lsp_document_diagnostics, {noremap=true, silent=true, buffer=bufnr})
--- 	end,
---
---   settings = {
---     Lua = {
---       telemetry = {
---         enable = false,
---       },
---       runtime = {
---         version = 'LuaJIT',
---       },
---       diagnostics = {
---         globals = {'vim', 'ngx', 'describe', 'it'},
---       },
---       workspace = {
---         checkThirdParty = false
---       }
---     }
---   }
--- })
 
 -- https://github.com/Shatur/neovim-ayu
 vim.cmd 'colorscheme ayu-mirage'
