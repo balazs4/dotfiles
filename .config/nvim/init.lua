304425726e3b405cc33bbc54302293c7b35db765
balazs4 <balazs4@users.noreply.github.com>
2023-07-24 17:42:20 +0200

    1690213340@mcbpro

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,8 +24,8 @@ end, { noremap = true, silent = true })
 require('fzf-lua').setup { 'default', winopts = { fullscreen = false, preview = { layout = 'vertical' } } }
 vim.keymap.set('n', '<leader>]', require('fzf-lua').files, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>[', require('fzf-lua').buffers, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader><leader>', require('fzf-lua').builtin, { noremap = true, silent = true })
-vim.keymap.set('n', '``', require('fzf-lua').resume, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader><leader>', require('fzf-lua').grep_project, { noremap = true, silent = true })
+vim.keymap.set('n', '``', require('fzf-lua').builtin, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>=', require('fzf-lua').blines, { noremap = true, silent = true })
 
 -- https://github.com/hrsh7th/cmp-nvim-lsp
