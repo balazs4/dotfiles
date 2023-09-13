9d62739262d6fbeb306ef6c5401e784fdcc595e0
balazs4 <balazs4@users.noreply.github.com>
2023-09-13 10:42:39 +0200

    1694594559@mcbpro

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,16 +21,15 @@ vim.keymap.set('n', '<leader>g', function()
 end, { noremap = true, silent = true })
 
 -- https://github.com/ibhagwan/fzf-lua
-require('fzf-lua').setup { 'default', winopts = { fullscreen = false, preview = { layout = 'vertical' } } }
-vim.keymap.set('n', '<leader>-', require('fzf-lua').builtin, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>[', function() vim.cmd('bn') end, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>]', function() vim.cmd('bp') end, { noremap = true, silent = true })
+require('fzf-lua').setup { 'default', winopts = { fullscreen = true, preview = { layout = 'vertical' } } }
+vim.keymap.set('n', '<leader>=', require('fzf-lua').builtin, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader><leader>', require('fzf-lua').files, { noremap = true, silent = true })
 vim.keymap.set('n', '``', require('fzf-lua').buffers, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>`', require('fzf-lua').grep_project, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>-', require('fzf-lua').grep_project, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>w', require('fzf-lua').grep_cword, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>W', require('fzf-lua').grep_cWORD, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>=', require('fzf-lua').blines, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>/', require('fzf-lua').blines, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>`', require('fzf-lua').git_status, { noremap = true, silent = true })
 
 -- https://github.com/hrsh7th/cmp-nvim-lsp
 -- https://github.com/hrsh7th/cmp-buffer
