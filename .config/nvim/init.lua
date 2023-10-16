ef53776be3ea3adde50f994e7a06eeb0338c7e94
balazs4 <balazs4@users.noreply.github.com>
2023-10-16 11:26:24 +0200

    1697448384@mcbpro

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -121,4 +121,4 @@ vim.g.user_emmet_leader_key='<C-Z>'
 
 -- https://github.com/tinted-theming/base16-vim
 vim.g.base16_background_transparent = 1
-vim.cmd("colorscheme base16-ayu-dark")
+vim.cmd("colorscheme base16-gruvbox-dark-hard")
