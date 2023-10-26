85afea52fbb91ed3e8381df024d7335b221d7e85
balazs4 <balazs4@users.noreply.github.com>
2023-10-26 11:39:33 +0200

    1698313173@mcbpro

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -102,10 +102,10 @@ require('fzf-lua').setup({
 vim.keymap.set('n', '<leader>/', require('fzf-lua').builtin, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader><leader>', require('fzf-lua').files, { noremap = true, silent = true })
 vim.keymap.set('n', '``', require('fzf-lua').buffers, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>=', require('fzf-lua').grep_project, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>-', require('fzf-lua').grep_project, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>w', require('fzf-lua').grep_cword, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>W', require('fzf-lua').grep_cWORD, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>-', require('fzf-lua').blines, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>=', require('fzf-lua').blines, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>`', require('fzf-lua').git_status, { noremap = true, silent = true })
 
 vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references, { noremap = true, silent = true})
