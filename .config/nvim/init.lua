a4feca1efda068cb49fef9c05b71faac7e253893
balazs4 <balazs4@users.noreply.github.com>
2023-11-05 17:25:27 +0100

    1699201527@carbon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,18 @@ vim.opt.listchars = "tab:  ,trail:·,eol: ,nbsp:_"
 vim.opt.cmdheight = 1
 vim.g.loaded_matchparen = 1
 
+-- theprimeagen
+vim.keymap.set("n", "J", 'mzJ`z')
+vim.keymap.set("n", "<C-d>", '<C-d>zz')
+vim.keymap.set("n", "<C-u>", '<C-u>zz')
+vim.keymap.set("n", "n", 'nzzzv')
+vim.keymap.set("n", "N", 'Nzzzv')
+vim.keymap.set("x", "<C-p>", "\"_dP")
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("n", "Q", "<nop>")
+
 vim.keymap.set('n', '<leader>g', function()
   local filename = string.gsub(vim.fn.expand('%'), os.getenv('PWD') or "", "")
   local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
@@ -26,40 +38,40 @@ vim.diagnostic.config({
   signs = false,
   update_in_insert = false,
   underline = false,
-  virtual_text = { severity = vim.diagnostic.severity.ERROR , spacing = 4 }
+  virtual_text = { severity = vim.diagnostic.severity.ERROR, spacing = 4 }
 })
 
 local function lsp(pattern, cmd, project_file, setup)
   vim.api.nvim_create_autocmd('FileType', {
     pattern = pattern,
     callback = function()
-      local root_dir = vim.fs.dirname( 
+      local root_dir = vim.fs.dirname(
         vim.fs.find(project_file and project_file or vim.fn.expand('%'), { upward = true })[1]
       )
 
       local client = vim.lsp.start({ name = pattern[1], cmd = cmd, root_dir = root_dir })
       vim.lsp.buf_attach_client(0, client)
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true})
-      vim.keymap.set('n', '<leader>p', function() vim.lsp.buf.format({ async = true }) end, { noremap = true, silent = true} )
-      vim.keymap.set('n', 'gR', vim.lsp.buf.rename, { noremap = true, silent = true})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>p', function() vim.lsp.buf.format({ async = true }) end,
+        { noremap = true, silent = true })
+      vim.keymap.set('n', 'gR', vim.lsp.buf.rename, { noremap = true, silent = true })
       vim.keymap.set('n', '<leader>T', vim.diagnostic.open_float, { noremap = true, silent = true })
 
       if setup then setup(client) end
-
     end
   })
 end
 
-lsp({'go'}, {'gopls'}, {'go.mod'})
-lsp({'templ'}, {'templ', 'lsp'}, {'go.mod'})
-lsp({'lua'}, {'lua-language-server'}, {'.luarc.json'})
-lsp({'rust'}, {'rust-analyzer'}, {'Cargo.toml'})
+lsp({ 'go' }, { 'gopls' }, { 'go.mod' })
+lsp({ 'templ' }, { 'templ', 'lsp' }, { 'go.mod' })
+lsp({ 'lua' }, { 'lua-language-server' }, { '.luarc.json' })
+lsp({ 'rust' }, { 'rust-analyzer' }, { 'Cargo.toml' })
 --carbon lsp({'typescript'}, {'deno', 'lsp'}, {'deno.json'})
-lsp({'typescript', 'typescriptreact'}, {'typescript-language-server', '--stdio'}, {'tsconfig.json'},
+lsp({ 'typescript', 'typescriptreact' }, { 'typescript-language-server', '--stdio' }, { 'tsconfig.json' },
   function()
     pcall(vim.keymap.del, 'n', '<leader>p')
-    vim.keymap.set('n', '<leader>p', function() vim.cmd(':PrettierAsync') end, { noremap = true, silent = true} )
+    vim.keymap.set('n', '<leader>p', function() vim.cmd(':PrettierAsync') end, { noremap = true, silent = true })
 
     vim.keymap.set('n', '<leader>t', function()
       local filename = vim.fn.expand('%')
@@ -109,8 +121,8 @@ vim.keymap.set('n', '<leader>W', require('fzf-lua').grep_cWORD, { noremap = true
 vim.keymap.set('n', '<leader>=', require('fzf-lua').blines, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>`', require('fzf-lua').git_status, { noremap = true, silent = true })
 
-vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references, { noremap = true, silent = true})
-vim.keymap.set('n', 'ga', require('fzf-lua').lsp_code_actions, { noremap = true, silent = true})
+vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references, { noremap = true, silent = true })
+vim.keymap.set('n', 'ga', require('fzf-lua').lsp_code_actions, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>b', require('fzf-lua').lsp_document_diagnostics, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>y', require('fzf-lua').lsp_document_symbols, { noremap = true, silent = true })
 
@@ -126,7 +138,7 @@ vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 require('tree-sitter-templ').setup({ highlight = { enable = true } })
 
 -- https://github.com/mattn/emmet-vim
-vim.g.user_emmet_leader_key='<C-Z>'
+vim.g.user_emmet_leader_key = '<C-Z>'
 
 -- https://github.com/prettier/vim-prettier
 
