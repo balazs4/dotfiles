3e6741818fbbca49eab66f18e6905002147e8b4b
balazs4 <balazs4@users.noreply.github.com>
2023-11-07 17:00:29 +0100

    1699372829@mcbpro

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,12 +17,6 @@ vim.opt.cmdheight = 1
 vim.g.loaded_matchparen = 1
 
 -- theprimeagen
-vim.keymap.set("n", "J", 'mzJ`z')
-vim.keymap.set("n", "<C-d>", '<C-d>zz')
-vim.keymap.set("n", "<C-u>", '<C-u>zz')
-vim.keymap.set("n", "n", 'nzzzv')
-vim.keymap.set("n", "N", 'Nzzzv')
-vim.keymap.set("x", "<C-p>", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
