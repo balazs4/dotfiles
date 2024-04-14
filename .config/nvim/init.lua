478b06218cac6d5ce25b24d1cced06e8a184117c
balazs4 <balazs4@users.noreply.github.com>
2024-04-14 23:18:01 +0200

    1713129481@carbon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,19 +40,29 @@ vim.diagnostic.config({
 
 --- lsp function
 --- @param pattern table filetypes
---- @param cmd table lsp command to execute with params
---- @param project_file table project file in root of project
---- @param setup function optional setup function to call on_attach-ish manner
-local function lsp(pattern, cmd, project_file, setup)
+--- @param project_to_lsp table lsp command to execute with params
+local function lsp(pattern, project_to_lsp)
   vim.api.nvim_create_autocmd('FileType', {
     pattern = pattern,
     callback = function()
-      local root_dir = vim.fs.dirname(
-        vim.fs.find(project_file and project_file or vim.fn.expand('%'), { upward = true })[1]
-      )
+      local project_file
+      local cmd
+      local root_dir
+      for project, command in pairs(project_to_lsp) do
+        local f = vim.fs.find(project, { upward = true })[1]
+        if f then
+          root_dir = vim.fs.dirname(f)
+          cmd = command
+          project_file = project
+          break
+        end
+      end
 
-      local client = vim.lsp.start({ name = pattern[1], cmd = cmd, root_dir = root_dir })
+      if cmd == nil then return end
+
+      local client = vim.lsp.start({ name = project_file, cmd = cmd, root_dir = root_dir })
       vim.lsp.buf_attach_client(0, client)
+      print(table.concat(cmd," "), "<<", root_dir)
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
       vim.keymap.set('n', '<leader>p', function() vim.lsp.buf.format({ async = true }) end,
@@ -60,43 +70,48 @@ local function lsp(pattern, cmd, project_file, setup)
       vim.keymap.set('n', 'gR', vim.lsp.buf.rename, { noremap = true, silent = true })
       vim.keymap.set('n', '<leader>T', vim.diagnostic.open_float, { noremap = true, silent = true })
 
-      if setup then setup(client) end
+      if project_file == 'tsconfig.json'
+      then
+        local function filename(test)
+          local buffer = vim.fn.expand('%')
+          if buffer:sub(-string.len('test.ts')) == 'test.ts' then
+            if test == true then
+              return buffer
+            else
+              return string.gsub(buffer, ".test.ts$", ".ts")
+            end
+          end
+          return string.gsub(buffer, ".ts$", ".test.ts")
+        end
+
+        pcall(vim.keymap.del, 'n', '<leader>p')
+        vim.keymap.set('n', '<leader>p', function() vim.cmd(':PrettierAsync') end, { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>t', function() vim.cmd('vsplit ' .. filename(false)) end,
+          { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>r',
+          function() vim.cmd('! tmux split-window -h zsh -i -c "npmw test ' .. filename(true) .. '"') end,
+          { noremap = true, silent = true })
+      end
     end
   })
 end
 
 vim.api.nvim_create_user_command("LspInfo", function() vim.cmd(":lua= vim.lsp.get_active_clients()") end, {})
 
-lsp({ 'gleam' }, { 'gleam', 'lsp' }, { 'gleam.toml' })
-lsp({ 'go' }, { 'gopls' }, { 'go.mod' })
-lsp({ 'templ' }, { 'templ', 'lsp' }, { 'go.mod' })
-lsp({ 'lua' }, { 'lua-language-server' }, { '.luarc.json' })
-lsp({ 'rust' }, { 'rust-analyzer' }, { 'Cargo.toml' })
-lsp({ 'terraform' }, { 'terraform-ls', 'serve' }, { '.terrform.lock.hcl' })
-lsp({ 'typescript', 'typescriptreact' }, { 'bun', 'x', 'typescript-language-server', '--stdio' },
-  { 'tsconfig.json', 'jsconfig.json' },
-  function()
-    local function filename(test)
-      local buffer = vim.fn.expand('%')
-      if buffer:sub(-string.len('test.ts')) == 'test.ts' then
-        if test == true then
-          return buffer
-        else
-          return string.gsub(buffer, ".test.ts$", ".ts")
-        end
-      end
-      return string.gsub(buffer, ".ts$", ".test.ts")
-    end
+lsp({ 'lua' }, { ['.luarc.json'] = { 'lua-language-server' } })
 
-    pcall(vim.keymap.del, 'n', '<leader>p')
-    vim.keymap.set('n', '<leader>p', function() vim.cmd(':PrettierAsync') end, { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>t', function() vim.cmd('vsplit ' .. filename(false)) end,
-      { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>r',
-      function() vim.cmd('! tmux split-window -h zsh -i -c "npmw test ' .. filename(true) .. '"') end,
-      { noremap = true, silent = true })
-  end
-)
+lsp({ 'go' }, { ['go.mod'] = { 'gopls' } })
+lsp({ 'templ' }, { ['go.mod'] = { 'templ', 'lsp' } })
+
+lsp({ 'typescript', 'javascript', 'javascriptreact', 'typescriptreact' }, {
+  ['tsconfig.json'] = { 'bun', 'x', 'typescript-language-server', '--stdio' },
+  ['jsconfig.json'] = { 'bun', 'x', 'typescript-language-server', '--stdio' },
+  ['deno.json'] = { 'deno', 'lsp' },
+})
+
+lsp({ 'rust' }, { ['Cargo.toml'] = { 'rust-analyzer' } })
+lsp({ 'terraform' }, { ['.terrform.lock.hcl'] = { 'terraform-ls', 'serve' } })
+lsp({ 'gleam' }, { ['gleam.toml'] = { 'gleam', 'lsp' } })
 
 -- https://github.com/ibhagwan/fzf-lua
 require('fzf-lua').setup({
