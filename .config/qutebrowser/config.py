5adb92f69f6c377d1eb4f59200904c50922c76e2
balazs4 <balazs4@users.noreply.github.com>
2025-04-21 13:18:34 +0200

    add: ./.config/qutebrowser/config.py

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,12 @@
+config.load_autoconfig();
+
+c.url.searchengines = {
+    'DEFAULT':  'https://start.duckduckgo.com/html/?q={}',
+    '!d':       'https://start.duckduckgo.com/?q={}',
+    '!dw':      'https://thefreedictionary.com/{}',
+    '!gh':      'https://github.com/search?o=desc&q={}&s=stars',
+    '!gist':    'https://gist.github.com/search?q={}',
+    '!r':       'https://old.reddit.com/search?q={}',
+    '!w':       'https://en.wikipedia.org/wiki/{}',
+    '!yt':      'https://www.youtube.com/results?search_query={}'
+}
