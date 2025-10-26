dcfd678eb17d60d645e18057f111b3237fc3dabd
balazs4 <balazs4@users.noreply.github.com>
2025-10-26 20:15:08 +0100

    carbon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,6 +38,17 @@ vim.keymap.set('n', '<leader>w', ':silent grep <cword>| copen <CR>')
 vim.keymap.set('n', '`', ':buffers<CR>:buffer ')
 vim.keymap.set('v', '<C-y>,', ':!emmet<CR> | ==')
 
+vim.keymap.set('n', '<leader>a', function()
+  vim.cmd("argadd %")
+  vim.cmd("argdedup")
+end)
+vim.keymap.set('n', '<leader>e', function() vim.cmd.args() end)
+vim.keymap.set('n', '<C-1>', function() vim.cmd("silent! 1argument") end)
+vim.keymap.set('n', '<C-2>', function() vim.cmd("silent! 2argument") end)
+vim.keymap.set('n', '<C-3>', function() vim.cmd("silent! 3argument") end)
+vim.keymap.set('n', '<C-4>', function() vim.cmd("silent! 4argument") end)
+
+
 if os.getenv('PWD') == string.format('%s/.files', os.getenv('HOME')) then
   vim.keymap.set('n', '<leader><cr>', ':w | !make $HOME/%<CR>')
 end
