b921014caf8e27f55277be93beafc08f9a9bc879
balazs4 <balazs4@users.noreply.github.com>
2026-02-02 18:34:02 +0100

    1770053642@carbon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -150,7 +150,7 @@ vim.lsp.config('gopls', {
 })
 
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" }
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" }
 })
 require("nvim-treesitter.configs").setup({
   auto_install = false,
